c5814d61e53b5aeed24c232238c0e56586a6122e
Aaron Shim <aaron.k.shim@gmail.com>
2016-11-10 00:00:48 -0800

    fixed error handling

--- server.rb
+++ server.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'yaml'
 require 'curb'
 
+# error handling
+set :show_exceptions, :after_handler
+
 # If there is a file, we set env variables (QUICK HACK)
 begin
   config = YAML.load(IO.read('config.yml'))
@@ -17,33 +20,40 @@ post '/isbn' do
   # do the API search
   http = Curl.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{params[:isbn]}&key=#{ENV['GOOGLE_BOOKS_API_KEY']}")
 
-  # deal with the data we got
-  return if response['totalItems'] == 0 # how do we send error codes?
-  response = JSON.parse(http.body_str)
-  puts http.body_str # hopefully we can see the response code?
-  book = response['items'].first
-  isbn_10 = nil
-  isbn_13 = nil
-  book['volumeInfo']['industryIdentifiers'].each do |h|
-    if h['type'] == 'ISBN_10'
-      isbn_10 = h['identifier']
-    elsif h['type'] == 'ISBN_13'
-      isbn_13 = h['identifier']
+  begin
+    # deal with the data we got
+    response = JSON.parse(http.body_str)
+    puts http.body_str # hopefully we can see the response code?
+    raise "API returned an error: #{response['error']['message']}" if response['error']
+    raise 'No items found' if response['totalItems'] == 0
+    book = response['items'].first
+    isbn_10 = nil
+    isbn_13 = nil
+    book['volumeInfo']['industryIdentifiers'].each do |h|
+      if h['type'] == 'ISBN_10'
+        isbn_10 = h['identifier']
+      elsif h['type'] == 'ISBN_13'
+        isbn_13 = h['identifier']
+      end
     end
-  end
-  puts JSON.pretty_generate(book)
+    puts JSON.pretty_generate(book)
 
-  # give it back to the frontend
-  content_type :json
-  {
-    num: response['totalItems'],
-    google_books_id: book['id'],
-    title: book['volumeInfo']['title'],
-    authors: book['volumeInfo']['authors'].map { |x| x.gsub(',','') }.join(','),
-    date: book['volumeInfo']['publishedDate'],
-    isbn_10: isbn_10,
-    isbn_13: isbn_13,
-    pages: book['volumeInfo']['pageCount'],
-    image_link: book['volumeInfo']['imageLinks']['thumbnail']
-  }.to_json
+    # give it back to the frontend
+    status 200
+    content_type :json
+    {
+      num: response['totalItems'],
+      google_books_id: book['id'],
+      title: book['volumeInfo']['title'],
+      authors: book['volumeInfo']['authors'].map { |x| x.gsub(',','') }.join(','),
+      date: book['volumeInfo']['publishedDate'],
+      isbn_10: isbn_10,
+      isbn_13: isbn_13,
+      pages: book['volumeInfo']['pageCount'],
+      image_link: book['volumeInfo']['imageLinks']['thumbnail']
+    }.to_json
+  rescue RuntimeError => e
+    status 404
+    return e.message
+  end
 end
